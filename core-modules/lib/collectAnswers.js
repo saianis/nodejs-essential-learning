2626c67826afd9adc2b28b3c0180c275d5f43e7d
saianis <anish@Anishs-MacBook-Pro.local>
2020-04-04 03:46:10 +0530

    refactored with event listeners

--- core-modules/lib/collectAnswers.js
+++ core-modules/lib/collectAnswers.js
@@ -1,23 +1,32 @@
 const readline = require("readline");
+const { EventEmitter } = require("events");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 module.exports = (questions, done = f => f) => {
-    const answers = [];
+  const answers = [];
+  const [firstQuestion] = questions;
+  const emitter = new EventEmitter();
 
-    const [firstQuestion] = questions;
-
-    const questionAnswered = answer => {
-        answers.push(answer);
-        if (answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered);
-        } else {
-            done(answers);
-        }
+  const questionAnswered = answer => {
+    emitter.emit("answer", answer);
+    answers.push(answer);
+    if (answers.length < questions.length) {
+      emitter.emit("ask", questions[answers.length]);
+      rl.question(questions[answers.length], questionAnswered);
+    } else {
+      emitter.emit("complete", answers);
+      done(answers);
     }
+  };
 
+  process.nextTick(() => {
+    emitter.emit("ask", firstQuestion);
     rl.question(firstQuestion, questionAnswered);
-}
+  });
+
+  return emitter;
+};
